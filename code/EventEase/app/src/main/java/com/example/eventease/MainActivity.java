a92052f2c7f6eac18f767fb5092cbe6c82ecd7c7
Swayam1129 <swayam2@ualberta.ca>
2025-10-26 03:12:04 -0600

    Add Base Android Project

---
+++ code/EventEase/app/src/main/java/com/example/eventease/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.eventease;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
