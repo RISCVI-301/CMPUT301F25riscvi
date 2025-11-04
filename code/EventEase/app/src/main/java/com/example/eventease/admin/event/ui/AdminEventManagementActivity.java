806759526589222ae4380b0550dc2de321da4887
M-Affan-Nazir <m.affann80@hotmail.com>
2025-11-04 15:49:45 -0700

    Added Admin Event Management Activity file

---
+++ code/EventEase/app/src/main/java/com/example/eventease/admin/event/ui/AdminEventManagementActivity.java
@@ -0,0 +1,18 @@
+package com.example.eventease.admin.event.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.eventease.R;
+
+
+public class AdminEventManagementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin_event_management);
+    }
+
+}
