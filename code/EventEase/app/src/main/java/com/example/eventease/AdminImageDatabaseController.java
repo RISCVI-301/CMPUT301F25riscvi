eada3d792014a11199948a85d63bd3ebc431f6f9
M-Affan-Nazir <m.affann80@hotmail.com>
2025-11-04 14:08:36 -0700

    Added Database Controller

---
+++ code/EventEase/app/src/main/java/com/example/eventease/AdminImageDatabaseController.java
@@ -0,0 +1,16 @@
+package com.example.eventease;
+
+import java.util.Collections;
+import java.util.List;
+
+public class AdminImageDatabaseController {
+
+    public List<String> getImageLinks() {
+        List<String> urls = Collections.emptyList();
+        return urls;
+    }
+
+    public boolean deleteImage() {
+        return true;
+    }
+}
