74a2c528bd50d2b892be6c8539e676207a5a7158
M-Affan-Nazir <m.affann80@hotmail.com>
2025-11-04 15:52:41 -0700

    Update MainActivity to navigate to AdminEventManagementActivity

--- code/EventEase/app/src/main/java/com/example/eventease/MainActivity.java
+++ code/EventEase/app/src/main/java/com/example/eventease/MainActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.eventease.admin.event.ui.AdminEventManagementActivity;
 import com.example.eventease.admin.image.ui.AdminImageManagementActivity;
 import com.google.firebase.FirebaseApp;
 
@@ -20,9 +21,14 @@ public class MainActivity extends AppCompatActivity {
         Log.d("FirebaseTest", "FirebaseApp initialized: " + (FirebaseApp.getApps(this).size() > 0));
 
 
-        // Hand off to AdminImageManagementActivity
-        Intent intent = new Intent(this, AdminImageManagementActivity.class);
+//        // Hand off to AdminImageManagementActivity
+//        Intent intent = new Intent(this, AdminImageManagementActivity.class);
+//        startActivity(intent);
+//        finish(); // Finish MainActivity so 'Back' does not come back here.
+
+        // Hand off to AdminEventManagement Activity
+        Intent intent = new Intent(this, AdminEventManagementActivity.class);
         startActivity(intent);
-        finish(); // Finish MainActivity so 'Back' does not come back here.
+        finish();
     }
 }
