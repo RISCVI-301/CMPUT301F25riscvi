21ff97a54643838d43dba76640087c764a1176b4
Muhammad Zain <muhammadzainasad6@gmail.com>
2025-11-04 02:41:27 -0700

    build: update SDK and add Firebase/Glide/Material deps

--- code/EventEase/app/build.gradle.kts
+++ code/EventEase/app/build.gradle.kts
@@ -42,6 +42,8 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation(libs.navigation.ui)
     implementation(libs.firebase.firestore)
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
