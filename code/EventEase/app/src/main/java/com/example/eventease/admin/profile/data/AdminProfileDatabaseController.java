90590e83c67ce8ef865d8db70e60576953d7f7a8
M-Affan-Nazir <m.affann80@hotmail.com>
2025-11-28 23:52:03 -0700

    Added Failure listener - simply log using LogCat

--- code/EventEase/app/src/main/java/com/example/eventease/admin/profile/data/AdminProfileDatabaseController.java
+++ code/EventEase/app/src/main/java/com/example/eventease/admin/profile/data/AdminProfileDatabaseController.java
@@ -205,6 +205,7 @@ public class AdminProfileDatabaseController {
 
     }
 
+    // integration remaining !!!!
     private void removeUserFromEventLists(@NonNull String uid) {
         if (uid == null || uid.trim().isEmpty()) {
             Log.w(TAG, "removeUserFromEventLists: UID is null or empty, skipping");
